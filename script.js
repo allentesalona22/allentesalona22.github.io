084e94470dfd3f72769e7b8abebe33edc9aac8e2
Allen Godwin Tesalona <allentesalona22@gmail.com>
2026-09-22 17:07:03 +0800

    added google play mxs and gg

--- script.js
+++ script.js
@@ -8,9 +8,12 @@ document.querySelectorAll(".reveal").forEach(el => observer.observe(el));
 
 const menu = document.querySelector(".menu");
 const nav = document.querySelector(".nav nav");
+
 menu?.addEventListener("click", () => {
   const open = nav.style.display === "flex";
+
   nav.style.display = open ? "" : "flex";
+
   if (!open) {
     nav.style.position = "absolute";
     nav.style.top = "70px";
